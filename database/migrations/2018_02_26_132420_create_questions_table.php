b769af479930e02c474a50bd9ebb4a650be8b56c
unknown <daveadjei@yahoo.co.uk>
2018-02-26 15:08:17 +0100

    added migration files for tables

---
+++ database/migrations/2018_02_26_132420_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id'); // adds id, AI, PK
+            $table->integer('user_id'); // adds user_id (int (11))
+            $table->string('title', 255);//adds title (varchar(255))
+            $table->text('text');// adds text(TEXT)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
