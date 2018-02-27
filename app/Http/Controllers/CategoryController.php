151995e3f2feac3d8ab3655473c485ad127da8dd
Tomas <tl@premiato.org>
2018-02-27 11:15:14 +0100

    cat controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $categories = DB::table('categories')->get();//select all rows from the table 'categories'
+
+        $view = view('categories/index'); //prepare the view
+
+        $view->categories = $categories; //insert the selected categories into the view
+
+        return $view; //return the view
+    }
+
+}
