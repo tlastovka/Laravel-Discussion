6cddd681ef7426d9ea36180dfd1b6cf0a0a34687
Tomas <tl@premiato.org>
2018-02-27 13:31:24 +0100

    change in the category conetroller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -18,19 +18,4 @@ class CategoryController extends Controller
     }
 
 
-    public function index()
-    {
-        // select all rows from table categories
-        $categories = DB::table('categories')->get();
-
-// prepare the view
-        $view = view('categories/index');
-
-// insert the selected categories into the view
-        $view->categories = $categories;
-
-// return the view
-        return $view;
-    }
-
 }
