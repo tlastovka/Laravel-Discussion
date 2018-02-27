793b49c1e83d8c62351a5cea978da0c7e1d79a7c
Tomas <tl@premiato.org>
2018-02-27 13:16:59 +0100

    13:16

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,5 +7,30 @@ use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
+    public function index(){
+        $categories = DB::table('categories')->get();//select all rows from the table 'categories'
+
+        $view = view('categories/index'); //prepare the view
+
+        $view->categories = $categories; //insert the selected categories into the view
+
+        return $view; //return the view
+    }
+
+
+    public function index()
+    {
+        // select all rows from table categories
+        $categories = DB::table('categories')->get();
+
+// prepare the view
+        $view = view('categories/index');
+
+// insert the selected categories into the view
+        $view->categories = $categories;
+
+// return the view
+        return $view;
+    }
 
 }
